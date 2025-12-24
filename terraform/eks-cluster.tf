b3fc2782b0228898800d3acb820c6d3e8f9ad825
jamesstop1201 <jamesstop1201@gmail.com>
2025-12-24 16:43:25 +0800

    refactor: Enhance comments for clarity in EKS cluster configuration

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -7,11 +7,14 @@ module "eks" {
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
+
+  # 允許透過網際網路管理叢集 (kubectl 可直接從電腦連線)
+  # 如果你使用 GitHub Actions 或外部的部署工具，通常需要開啟此選項才能將程式碼派送到叢集內。
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
+    # AL2 (Amazon Linux 2)  x86_64 架構
   }
 
   eks_managed_node_groups = {
@@ -32,7 +35,7 @@ module "eks" {
 
       min_size     = 1
       max_size     = 2
-      desired_size = 1
+      desired_size = 1 # 平時維持 1 台運作
     }
   }
 }
